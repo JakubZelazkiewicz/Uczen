91611fa21fd9ac3c1b5db7ac1cff3ba188219289
kZelazkiewicz <kuba.totu.totam@gmail.com>
2018-01-09 20:02:00 +0100

    Projekt java uczniowie i oceny wykonane zadania 1-3 -repozytorium -generowanie ocen + średnie ucznów -wczytanie ocen z pliku tekstowego

--- Uczen.java
+++ Uczen.java
@@ -1,43 +1,67 @@
 package com.company;
 
+import java.io.*;
+
+
 public class Uczen {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+
+            File UczniowieOceny = new File("C:\\Users\\Kuba\\Desktop\\iSA\\jdqz1-git-master\\Uczen\\src\\com\\company\\UczniowieOceny.txt");
+            FileReader czytelnikFR = new FileReader(UczniowieOceny);
+            BufferedReader czytelnik = new BufferedReader(czytelnikFR); {
+
+                String wiersz =null;
+        int nrLoop = 0;
+        while((wiersz = czytelnik.readLine())!=null ){
+            nrLoop = nrLoop+1;
+
+                    System.out.println(nrLoop + "," + wiersz);
+                }
+                czytelnik.close();
+            System.out.println("");
+            System.out.println("");
+                }
+
+
     Uczen uczen1 = new Uczen("Monika", "M");
     Uczen uczen2 = new Uczen("Agnieszka", "M");
     Uczen uczen3 = new Uczen("Kamil", "G");
 
-        // Oceny uczeni1
+        //Oceny uczen1
         System.out.println("Wyniki Moniki");
-        System.out.println("Srednia ocen Moniki to " + uczen1.srednia());
-        System.out.println(" Najniższa ocena Moniki to " + uczen1.OcenaMin());
-        System.out.println("Najwyższa ocena Moniki to " + uczen1.OcenaMax());
+         System.out.println("Srednia ocen Moniki to " + uczen1.srednia());
+         System.out.println("Najniższa ocena Moniki to " + uczen1.OcenaMin());
+         System.out.println("Najwyższa ocena Moniki to " + uczen1.OcenaMax());
         System.out.println();
-        // Oceny uczeni2
+         //Oceny uczen2
         System.out.println("Wyniki Agnieszki");
         System.out.println("Srednia ocen Agnieszki to " + uczen2.srednia());
-        System.out.println(" Najniższa ocena Agnieszki to " + uczen2.OcenaMin());
+        System.out.println("Najniższa ocena Agnieszki to " + uczen2.OcenaMin());
         System.out.println("Najwyższa ocena Agnieszki to " + uczen2.OcenaMax());
         System.out.println();
-        // Oceny uczeni3
+         //Oceny uczen3
         System.out.println("Wyniki Kamila");
         System.out.println("Srednia ocen Kamila to " + uczen3.srednia());
-        System.out.println(" Najniższa ocena Kamila to " + uczen3.OcenaMin());
+        System.out.println("Najniższa ocena Kamila to " + uczen3.OcenaMin());
         System.out.println("Najwyższa ocena Kamila to " + uczen3.OcenaMax());
+        System.out.println();
 
-    }
+
+
+         }
+
     int iloscOcen = 6;
     String imie;
     String nazwisko;
     int [] oceny;
-
 
     public Uczen(String imie, String nazwisko) {
         this.imie = imie;
         this.nazwisko = nazwisko;
         this.oceny = tabelaOcen();
     }
-
 
     public static int [] tabelaOcen() {
         int[] oceny = new int[6]; // deklaracja pustej tablicy
@@ -49,14 +73,13 @@ public class Uczen {
         return oceny;
     }
 
-
     public double srednia() {
-        int a = 0;
+        double a = 0;
         for(int i=0; i<this.iloscOcen; i++){
-        int b = this.oceny[i];
+        double b = this.oceny[i];
         a=a+b;
     }
-    return 1.0*a/6;
+    return a/6;
 }
     public int OcenaMin() {
         int Min=oceny[0];
@@ -78,4 +101,8 @@ public class Uczen {
         }
         return Max;
     }
+
+
+
+
     }
